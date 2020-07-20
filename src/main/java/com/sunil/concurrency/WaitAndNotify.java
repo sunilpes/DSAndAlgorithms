8f0a2a83f7aa045f601cb4164e5d7f8f49e1ba7f
sunil <sunil.slvphsn@gmail.com>
2020-07-20 14:22:07 +0530

    wait and notify

---
+++ src/main/java/com/sunil/concurrency/WaitAndNotify.java
@@ -0,0 +1,84 @@
+package com.sunil.concurrency;
+
+/**
+ * https://www.baeldung.com/java-wait-notify
+ */
+class Data {
+
+    // True, if receiver should wait
+    // False, if sender should wait
+    private boolean transfer = true;
+    private String packet;
+
+    public synchronized void send(String packet) throws InterruptedException {
+        while(!transfer) {
+            wait();
+        }
+        transfer = false;
+        this.packet = packet;
+        notify();
+    }
+
+    public synchronized String receive() throws InterruptedException {
+        while(transfer) {
+            wait();
+        }
+        transfer = true;
+        notify();
+        return packet;
+    }
+}
+
+
+public class WaitAndNotify {
+
+    public static void main(String[] args) {
+        Data dataObject = new Data();
+
+        Thread t1 = new Thread(new Runnable() {
+
+            private String[] data = {
+                    "First packet",
+                    "Second packet",
+                    "Third packet",
+                    "Fourth packet",
+                    "END"
+            };
+
+            @Override
+            public void run() {
+                try {
+                    for(String value: data) {
+                        dataObject.send(value);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (String value= dataObject.receive(); !"END".equals(value); value=dataObject.receive()) {
+                        System.out.println(value);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
